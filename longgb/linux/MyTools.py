8f6045b1236aee376d173a3841a2fa4e45cd35f5
longgb246 <lgb453476610@163.com>
2016-12-22 10:28:48 +0800

    修改tools

--- longgb/linux/MyTools.py
+++ longgb/linux/MyTools.py
@@ -1,13 +1,21 @@
 #-*- coding:utf-8 -*-
 import os
 import time
-
+import numpy as np
 
 def printruntime(t1):
+    '''
+    性能测试，运行时间
+    '''
     d = time.time() - t1
-    min_d = int(d / 60)
+    min_d = np.floor(d / 60)
     sec_d = d % 60
-    print 'Run Time is : {0} min {1:.4f} s'.format(min_d, sec_d)
+    hor_d = np.floor(min_d / 60)
+    if hor_d >0:
+        print 'Run Time is : {2} hours {0} min {1:.4f} s'.format(min_d, sec_d, hor_d)
+    else:
+        print 'Run Time is : {0} min {1:.4f} s'.format(min_d, sec_d)
+
 
 def pyhive(com_str, log_str):
     os.system('echo "{0}" >> {1} 2>&1;'.format('*'*50, log_str))
