2a72adb6895fa5c9ec0911da5e8195ea6619d503
sunwenxiu <sunwenxiu@jd.com>
2017-01-10 18:12:37 +0800

    check error

---
+++ longgb/Work/Allocation/Allocation_check/inv_check.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+import pandas as pd
+import numpy as np
+import os
+
+path = r'D:\Lgb'
+read_path = path + os.sep + 'table_sample_sku.csv'
+inv_data = pd.read_csv(read_path)
+
+
+
+
