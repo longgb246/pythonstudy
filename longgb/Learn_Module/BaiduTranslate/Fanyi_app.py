5cab2735e1cd9b4c039ee91cac99aba7121bf087
longgb246 <lgb453476610@163.com>
2017-02-23 00:56:53 +0800

    fanyi_app

--- longgb/Learn_Module/BaiduTranslate/Fanyi_app.py
+++ longgb/Learn_Module/BaiduTranslate/Fanyi_app.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
-import sys
 from PyQt4 import QtGui, QtCore
+import httplib
+import md5
+import urllib
+import random
+import chardet
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import json
+
+# ==================================== 配置翻译 ====================================
+appid = '20170222000039667'                     # 账号
+secretKey = 'IeZ2wtwnNYxszXPDYDzC'              # 密钥
+httpClient = None
+myurls = '/api/trans/vip/translate'
+
 
 
 class buttonRedrect(QtGui.QWidget):                       # 继承
@@ -11,47 +26,121 @@ class buttonRedrect(QtGui.QWidget):                       # 继承
     def initUI(self):
         # ===================================== 字体、提示 ===============================
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))    # 设置提示字体
-        self.setToolTip('This is a <b>QWidget</b> widget')      # 提示字体格式，可以使用html标签
+        # self.setToolTip('This is a <b>QWidget</b> widget')      # 提示字体格式，可以使用html标签
 
         # ===================================== 窗口 =====================================
         self.setWindowTitle(u'百度翻译')                        # 设置窗口名称
         self.setWindowIcon(QtGui.QIcon(r"D:\Life\Photo\favicon.ico"))       # 设置Icon的图标
-        self.setGeometry(300, 300, 300, 150)
+        self.setGeometry(300, 300, 600, 400)
         # self.resize(300, 150)                                 # 设置窗口大小
         # self.move(300, 300)                                   # 设置位置
-        # self.center()                                         # 设置居中，见下面自定义函数。
+        self.center()                                         # 设置居中，见下面自定义函数。
 
+        self.edit_text = QtGui.QLineEdit(self)
+        self.edit_text.move(20, 30)
+        self.edit_text.resize(460, 30)
 
-
+        self.edit_text2 = QtGui.QLineEdit(self)
+        self.edit_text2.move(20, 230)
+        self.edit_text2.resize(460, 30)
 
         # ===================================== 按钮 =====================================
-        self.okButton = QtGui.QPushButton("OK", self)           # 设置按钮
-        self.okButton.setToolTip('This is a <b>QPushButton</b> widget')     # 设置按钮提示
+        self.okButton = QtGui.QPushButton(u"翻译一下", self)           # 设置按钮
+        # self.okButton.mouseReleaseEvent = self.clickEvents           # 设置button的点击事件
+        self.okButton.mouseReleaseEvent = self.fanyi_en2ch  # 设置button的点击事件
+        self.okButton.move(500, 30)
+        self.okButton.resize(80, 30)
+        # self.okButton.setToolTip('This is a <b>QPushButton</b> widget')     # 设置按钮提示
         # self.okButton.resize(btn.sizeHint())                  # sizeHint()方法返回一个推荐的大小。
         # qbtn = QtGui.QPushButton('Quit', self)
         # qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)     # 按钮连接关闭的方法。关闭当前窗口
         # qbtn.clicked.connect(self.btnCloseEvent)
-        self.cancelButton = QtGui.QPushButton("Cancel")
+        # self.cancelButton = QtGui.QPushButton("Cancel")
+        self.okButton2 = QtGui.QPushButton(u"翻译一下", self)           # 设置按钮
+        # self.okButton.mouseReleaseEvent = self.clickEvents           # 设置button的点击事件
+        self.okButton2.mouseReleaseEvent = self.fanyi_ch2en  # 设置button的点击事件
+        self.okButton2.move(500, 230)
+        self.okButton2.resize(80, 30)
 
-
-
-
-        self.labels = QtGui.QLabel(u'点我试试！', self)
+        # ===================================== 标签 =====================================
+        self.labelEn2CH = QtGui.QLabel(u'英文 翻译为 中文', self)
+        self.labelEn2CH.move(20,10)
         # self.labels.setGeometry(50, 50, 150, 50)
         # self.labels.mouseReleaseEvent = self.events
+        self.labelCH2En = QtGui.QLabel(u'中文 翻译为 英文', self)
+        self.labelCH2En.move(20,200)
 
+        # ===================================== 文本框 ===================================
+        self.En2CH = QtGui.QTextEdit(u'  ', self)
+        self.En2CH.setReadOnly(True)
+        self.En2CH.resize(560,100)
+        self.En2CH.move(20,70)
 
-        hbox = QtGui.QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.okButton)
-        # hbox.addWidget(self.cancelButton)
-        vbox = QtGui.QVBoxLayout()
-        # vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
+        self.CH2En = QtGui.QTextEdit(u'  ', self)
+        self.CH2En.setReadOnly(True)
+        self.CH2En.resize(560,100)
+        self.CH2En.move(20,270)
 
-        self.okButton.mouseReleaseEvent = self.events
+        # 布局，未看。
+        # hbox = QtGui.QHBoxLayout()
+        # hbox.addStretch(1)
+        # hbox.addWidget(self.okButton)
+        # # hbox.addWidget(self.cancelButton)
+        # vbox = QtGui.QVBoxLayout()
+        # # vbox.addStretch(1)
+        # vbox.addLayout(hbox)
+        # self.setLayout(vbox)
 
+    def fanyi_en2ch(self, event):
+        fromLang = 'en'  # 从..语言翻译
+        toLang = 'zh'  # 翻译到..语言
+        self.fanyi(fromLang, toLang)
+
+    def fanyi_ch2en(self, event):
+        fromLang = 'zh'  # 从..语言翻译
+        toLang = 'en'  # 翻译到..语言
+        self.fanyi(fromLang, toLang)
+
+    def fanyi(self, fromLang, toLang):
+        q = str(self.edit_text2.text())
+        # q = 'apple'  # 翻译语句
+        salt = random.randint(32768, 65536)  # 随机数
+        sign = appid + q + str(salt) + secretKey  # sign
+        m1 = md5.new()
+        m1.update(sign)
+        sign = m1.hexdigest()  # md5
+        myurl = myurls + '?appid=' + appid + '&q=' + urllib.quote(q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+        try:
+            httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
+            httpClient.request('GET', myurl)
+            response = httpClient.getresponse()
+            response_dict2 = response.read()
+            response_dict2 = json.loads(response_dict2)
+            # print response_dict2
+            print response_dict2["trans_result"][0]["dst"]
+            result = response_dict2["trans_result"][0]["dst"]
+            # uni_result = result.decode("unicode-escape")
+            if fromLang == 'en':
+                print "en"
+                self.fanyi_show_en2ch(result)
+            else:
+                print "ch"
+                self.fanyi_show_ch2en(result)
+        except Exception, e:
+            print e
+        finally:
+            if httpClient:
+                httpClient.close()
+
+    def fanyi_show_ch2en(self, content):
+        self.CH2En.setText(content)
+        # self.En2CH.adjustSize()
+        self.CH2En.update()
+
+    def fanyi_show_en2ch(self, content):
+        self.En2CH.setText(content)
+        # self.En2CH.adjustSize()
+        self.En2CH.update()
 
     def center(self):
         '''
@@ -64,7 +153,7 @@ class buttonRedrect(QtGui.QWidget):                       # 继承
         qr.moveCenter(cp)                                       # 将矩形框架移至屏幕正中央，大小不变。
         self.move(qr.topLeft())                                 # 窗口移至矩形框架的左上角点，这样应用窗口就位于屏幕的中央了【注意部件的move都是左上角移动到某点】。
 
-    def events(self, event):
+    def clickEvents(self, event):
         '''
         点击事件，弹出提示信息。
         '''
