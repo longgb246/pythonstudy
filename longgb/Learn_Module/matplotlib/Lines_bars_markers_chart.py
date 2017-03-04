09e73c3718900621098aaee05ebbd0e6854aa650
longgb246 <lgb453476610@163.com>
2017-03-05 01:16:16 +0800

    matplotlibe 添加 linstyles、marker_fillstyle_reference、scatter_with_legend。

--- longgb/Learn_Module/matplotlib/Lines_bars_markers_chart.py
+++ longgb/Learn_Module/matplotlib/Lines_bars_markers_chart.py
@@ -97,7 +97,8 @@ def fill_demo_features():
     ax.set_ylim([y_min,y_max])
     plt.show()
 
-# 3、设置虚线点
+
+# 3、设置线(Line)、点(Marker)
 def line_demo_dash_control():
     x = np.linspace(0, 10, 500)
     dashes = [10, 5, 100, 5]        # 10 points 画, 5 不画, 100 画, 5 不画，点数根据 x 的linspace的那个决定
@@ -110,7 +111,9 @@ def line_demo_dash_control():
     line2, = ax2.plot(x, -1 * np.sin(x), dashes=[30, 5, 10, 5], label='Dashes set proactively')
     ax2.legend(loc='lower right')
     plt.show()
-# nice_repr 为有用函数，处理原unicode的编码变为str
+# 3.1、nice_repr 为有用函数，处理原unicode的编码变为str
+# 3.2、ax.margins(0.2)    ax.set_axis_off()    ax.text(-0.1, y, nice_repr(linestyle), **text_style)
+# text_style
 def line_styles_reference():
     color = 'cornflowerblue'
     points = np.ones(5)  # Draw 5 points for each line
@@ -133,6 +136,128 @@ def line_styles_reference():
         format_axes(ax)
         ax.set_title('line styles')
     plt.show()
+# 3.3、plt.tight_layout()   ax.annotate()   ax.set_xticks([])   ax.set_yticks()   ax.set_yticklabels()
+def linestyles():
+    from collections import OrderedDict             # 引入有序的字典，不改变字典的顺序
+    # from matplotlib.transforms import blended_transform_factory
+    linestyles = OrderedDict(
+        [('solid', (0, ())),
+         ('loosely dotted', (0, (1, 10))),
+         ('dotted', (0, (1, 5))),
+         ('densely dotted', (0, (1, 1))),
+         ('loosely dashed', (0, (5, 10))),
+         ('dashed', (0, (5, 5))),
+         ('densely dashed', (0, (5, 1))),
+         ('loosely dashdotted', (0, (3, 10, 1, 10))),
+         ('dashdotted', (0, (3, 5, 1, 5))),
+         ('densely dashdotted', (0, (3, 1, 1, 1))),
+         ('loosely dashdotdotted', (0, (3, 10, 1, 10, 1, 10))),
+         ('dashdotdotted', (0, (3, 5, 1, 5, 1, 5))),
+         ('densely dashdotdotted', (0, (3, 1, 1, 1, 1, 1)))])
+    # 开始画图
+    plt.figure(figsize=(10, 6))
+    ax = plt.subplot(111)
+    X, Y = np.linspace(0, 100, 10), np.zeros(10)
+    for i, (name, linestyle) in enumerate(linestyles.items()):
+        ax.plot(X, Y + i, linestyle=linestyle, linewidth=1.5, color='black')
+    ax.set_ylim(-0.5, len(linestyles) - 0.5)
+    # plt.yticks(np.arange(len(linestyles)), linestyles.keys())
+    ax.set_yticks(np.arange(len(linestyles)))
+    ax.set_yticklabels(linestyles.keys())
+    # plt.xticks([])
+    # ax.set_xticklabels([''])
+    ax.set_xticks([])
+    # reference_transform = blended_transform_factory(ax.transAxes, ax.transData)
+    for i, (name, linestyle) in enumerate(linestyles.items()):
+        ax.annotate(str(linestyle),
+                    xy=(0.0, i),
+                    # xycoords=reference_transform,
+                    xytext=(-6, -14),        # 标注的位置，(x,y)，x为正表示向右，负表示向左；y为正表示向上，负表示向下
+                    textcoords='offset points',     # 'offset pixels' 不知道是什么意思，但是这个属性必须有
+                    color="blue",
+                    fontsize=8,
+                    ha="right",              # right：以右为基准向左边开始写；left：以左为基准向右边开始写；
+                    family="monospace")
+    plt.tight_layout()                  # 使得布局可以全部布满，并且都显示出来
+    plt.show()
+    pass
+# text_style    marker_style    Line2D
+def marker_fillstyle_reference():
+    from matplotlib.lines import Line2D
+    def format_axes(ax):
+        ax.margins(0.2)
+        ax.set_axis_off()
+    def nice_repr(text):
+        return repr(text).lstrip('u')
+    points = np.ones(5)  # Draw 3 points for each line
+    text_style = dict(horizontalalignment='right', verticalalignment='center',
+                      fontsize=12, fontdict={'family': 'monospace'})
+    marker_style = dict(color='cornflowerblue', linestyle=':', marker='o',
+                        markersize=15, markerfacecoloralt='gray')
+    fig, ax = plt.subplots()
+    # Plot all fill styles.
+    for y, fill_style in enumerate(Line2D.fillStyles):
+        ax.text(-0.5, y, nice_repr(fill_style), **text_style)
+        ax.plot(y * points, fillstyle=fill_style, **marker_style)
+        format_axes(ax)
+        ax.set_title('fill style')
+    plt.show()
+    pass
+# text_style    marker_style    Line2D
+def marker_reference():
+    from six import iteritems
+    from matplotlib.lines import Line2D
+    def format_axes(ax):
+        ax.margins(0.2)
+        ax.set_axis_off()
+    def nice_repr(text):
+        return repr(text).lstrip('u')
+    def split_list(a_list):
+        i_half = len(a_list) // 2
+        return (a_list[:i_half], a_list[i_half:])
+    points = np.ones(3)  # Draw 3 points for each line
+    text_style = dict(horizontalalignment='right', verticalalignment='center',
+                      fontsize=12, fontdict={'family': 'monospace'})
+    marker_style = dict(linestyle=':', color='cornflowerblue', markersize=10)
+    fig, axes = plt.subplots(ncols=2)
+    unfilled_markers = [m for m, func in iteritems(Line2D.markers)
+                        if func != 'nothing' and m not in Line2D.filled_markers]
+    unfilled_markers = sorted(unfilled_markers,
+                              key=lambda x: (str(type(x)), str(x)))[::-1]
+    for ax, markers in zip(axes, split_list(unfilled_markers)):
+        for y, marker in enumerate(markers):
+            ax.text(-0.5, y, nice_repr(marker), **text_style)
+            ax.plot(y * points, marker=marker, **marker_style)
+            format_axes(ax)
+    fig.suptitle('un-filled markers', fontsize=14)
+    fig, axes = plt.subplots(ncols=2)
+    for ax, markers in zip(axes, split_list(Line2D.filled_markers)):
+        for y, marker in enumerate(markers):
+            ax.text(-0.5, y, nice_repr(marker), **text_style)
+            ax.plot(y * points, marker=marker, **marker_style)
+            format_axes(ax)
+    fig.suptitle('filled markers', fontsize=14)
+    plt.show()
+    pass
+
+
+# 4、散点图
+# 4.1、散点图(指定点大小，把点边界取消)+legend透明的
+def scatter_with_legend():
+    fig, ax = plt.subplots()
+    for color in ['red', 'green', 'blue']:
+        n = 750
+        x, y = np.random.rand(2, n)
+        scale = 200.0 * np.random.rand(n)
+        ax.scatter(x, y, c=color, s=scale, label=color, alpha=0.3
+                   ,edgecolors='none'   # 把边界取消
+                   )
+    ax.legend(framealpha=0.7)
+    # ax.legend()
+    ax.grid(True)
+    plt.show()
+
+
 
 
 
