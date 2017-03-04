f5b8257b5e18976a77bd0209f1d15eee4eee6b61
longgb246 <lgb453476610@163.com>
2017-03-04 17:33:13 +0800

    添加Tools文件夹，把翻译app添加进去。

--- longgb/Learn_Module/matplotlib/Lines_bars_markers_chart.py
+++ longgb/Learn_Module/matplotlib/Lines_bars_markers_chart.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# 画横着的 bar 图, ax.barh
+# 1、画横着的 bar 图, ax.barh
 def barh_demo():
     '''
     画横着的bar图
@@ -23,7 +23,7 @@ def barh_demo():
     ax.set_title('How fast do you want to go today?')
 
 
-# 画填充图, ax.fill
+# 2、画填充图, ax.fill
 def fill_demo():
     x = np.linspace(0, 1, 500)
     y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
@@ -37,7 +37,7 @@ def fill_between_demo():
     x = np.arange(0.0, 2, 0.01)
     y1 = np.sin(2 * np.pi * x)
     y2 = 1.2 * np.sin(4 * np.pi * x)
-
+    # -------------------------- 画两个函数之间的图 --------------------------
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)          # sharex 分享x轴坐标刻度，三个图都有x轴刻度，否则只有最下面的有
     # fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
     # 分开添加不可以指定是否共享同一 x 坐标轴。以后使用上面的形式开启图片。
@@ -47,7 +47,6 @@ def fill_between_demo():
     # ax2 = fig.add_subplot(312)
     # ax2.set_xticklabels([''])
     # ax3 = fig.add_subplot(313)
-
     ax1.fill_between(x, 0, y1)
     ax1.set_ylabel('between y1 and 0')
     ax2.fill_between(x, y1, 1)
@@ -55,6 +54,61 @@ def fill_between_demo():
     ax3.fill_between(x, y1, y2)
     ax3.set_ylabel('between y1 and y2')
     ax3.set_xlabel('x')
+    # -------------------------- 画两个函数(区分函数大小)的图 --------------------------
+    fig, (ax, ax1) = plt.subplots(2, 1, sharex=True)
+    ax.plot(x, y1, x, y2, color='black')
+    ax.fill_between(x, y1, y2, where=y2 >= y1, facecolor='green', interpolate=True)
+    ax.fill_between(x, y1, y2, where=y2 <= y1, facecolor='red', interpolate=True)
+    ax.set_title('fill between where')
+    y2 = np.ma.masked_greater(y2, 1.0)
+    ax1.plot(x, y1, x, y2, color='black')
+    ax1.fill_between(x, y1, y2, where=y2 >= y1, facecolor='green', interpolate=True)
+    ax1.fill_between(x, y1, y2, where=y2 <= y1, facecolor='red', interpolate=True)
+    ax1.set_title('Now regions with y2>1 are masked')
+    # 不知道画什么图
+    fig, ax = plt.subplots()
+    y = np.sin(4 * np.pi * x)
+    ax.plot(x, y, color='black')
+    # use the data coordinates for the x-axis and the axes coordinates for the y-axis
+    import matplotlib.transforms as mtransforms
+    trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
+    theta = 0.9
+    ax.axhline(theta, color='green', lw=2, alpha=0.5)
+    ax.axhline(-theta, color='red', lw=2, alpha=0.5)
+    ax.fill_between(x, 0, 1, where=y > theta, facecolor='green', alpha=0.5, transform=trans)
+    ax.fill_between(x, 0, 1, where=y < -theta, facecolor='red', alpha=0.5, transform=trans)
+def fill_demo_features():
+    x = np.linspace(0, 2*np.pi, 500)
+    x_max = np.max(x) + (np.max(x) - np.min(x))*0.05
+    x_min = np.min(x) - (np.max(x) - np.min(x))*0.05
+    y1 = np.sin(x)
+    y2 = np.sin(x*3)
+    y_max = np.max([y1,y2]) + (np.max([y1,y2]) - np.min([y1,y2]))*0.05
+    y_min = np.min([y1,y2]) - (np.max([y1,y2]) - np.min([y1,y2]))*0.05
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.fill(x, y1, 'b', alpha=0.3)
+    ax.fill(x, y2, 'r', alpha=0.3)
+    ax.set_xlim([x_min,x_max])
+    ax.set_ylim([y_min,y_max])
+    plt.show()
+
+def line_demo_dash_control():
+    x = np.linspace(0, 10, 500)
+    dashes = [10, 5, 100, 5]        # 10 points on, 5 off, 100 on, 5 off
+    fig = plt.figure(figsize=(18,7))
+    ax = fig.add_subplot(121)
+    line1, = ax.plot(x, np.sin(x), '--', linewidth=2, label='Dashes set retroactively')     # 之所以用“，”，是因为想直接取返回的list中的第一个元素
+    ax2 = fig.add_subplot(122)
+    line2, = ax2.plot(x, np.sin(x), '--', linewidth=2, label='Dashes set retroactively')  # 之所以用“，”，是因为想直接取返回的list中的第一个元素
+    line2.set_dashes(dashes)
+    line2, = ax.plot(x, -1 * np.sin(x), dashes=[30, 5, 10, 5], label='Dashes set proactively')
+
+
+    ax.legend(loc='lower right')
+    plt.show()
+
+
 
 if __name__ == '__main__':
     pass
---
+++ longgb/Tools/Fanyi_app.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtGui, QtCore
+import httplib
+import md5
+import urllib
+import random
+import chardet
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import json
+
+# ==================================== 配置翻译 ====================================
+appid = '20170222000039667'                     # 账号
+secretKey = 'IeZ2wtwnNYxszXPDYDzC'              # 密钥
+httpClient = None
+myurls = '/api/trans/vip/translate'
+
+
+class fanyiApp(QtGui.QWidget):                       # 继承
+    def __init__(self):
+        super(fanyiApp, self).__init__()               # 继承
+        self.initUI()
+
+    def initUI(self):
+        # ===================================== 字体、提示 ===============================
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))    # 设置提示字体
+        # self.setToolTip('This is a <b>QWidget</b> widget')      # 提示字体格式，可以使用html标签
+
+        # ===================================== 窗口 =====================================
+        self.setWindowTitle(u'百度翻译')                        # 设置窗口名称
+        # self.setWindowIcon(QtGui.QIcon(r"D:\Life\Photo\favicon.ico"))       # 设置Icon的图标
+        self.setWindowIcon(QtGui.QIcon(r"D:\Lgb\Self\favicon.ico"))       # 设置Icon的图标
+        self.setGeometry(300, 300, 600, 400)
+        # self.resize(300, 150)                                 # 设置窗口大小
+        # self.move(300, 300)                                   # 设置位置
+        self.center()                                         # 设置居中，见下面自定义函数。
+
+        self.edit_text = QtGui.QLineEdit(self)
+        self.edit_text.move(20, 30)
+        self.edit_text.resize(460, 30)
+
+        self.edit_text2 = QtGui.QLineEdit(self)
+        self.edit_text2.move(20, 230)
+        self.edit_text2.resize(460, 30)
+
+        # ===================================== 按钮 =====================================
+        self.okButton = QtGui.QPushButton(u"翻译一下", self)           # 设置按钮
+        # self.okButton.mouseReleaseEvent = self.clickEvents           # 设置button的点击事件
+        self.okButton.mouseReleaseEvent = self.fanyi_en2ch  # 设置button的点击事件
+        self.okButton.move(500, 30)
+        self.okButton.resize(80, 30)
+        # self.okButton.setToolTip('This is a <b>QPushButton</b> widget')     # 设置按钮提示
+        # self.okButton.resize(btn.sizeHint())                  # sizeHint()方法返回一个推荐的大小。
+        # qbtn = QtGui.QPushButton('Quit', self)
+        # qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)     # 按钮连接关闭的方法。关闭当前窗口
+        # qbtn.clicked.connect(self.btnCloseEvent)
+        # self.cancelButton = QtGui.QPushButton("Cancel")
+        self.okButton2 = QtGui.QPushButton(u"翻译一下", self)           # 设置按钮
+        # self.okButton.mouseReleaseEvent = self.clickEvents           # 设置button的点击事件
+        self.okButton2.mouseReleaseEvent = self.fanyi_ch2en  # 设置button的点击事件
+        self.okButton2.move(500, 230)
+        self.okButton2.resize(80, 30)
+
+        # ===================================== 标签 =====================================
+        self.labelEn2CH = QtGui.QLabel(u'[ 英文 ] 翻译为 [ 中文 ]', self)
+        self.labelEn2CH.move(20,10)
+        # self.labels.setGeometry(50, 50, 150, 50)
+        # self.labels.mouseReleaseEvent = self.events
+        self.labelCH2En = QtGui.QLabel(u'[ 中文 ] 翻译为 [ 英文 ]', self)
+        self.labelCH2En.move(20,200)
+
+        # ===================================== 文本框 ===================================
+        self.En2CH = QtGui.QTextEdit(u'  ', self)
+        self.En2CH.setReadOnly(True)
+        self.En2CH.resize(560,100)
+        self.En2CH.move(20,70)
+
+        self.CH2En = QtGui.QTextEdit(u'  ', self)
+        self.CH2En.setReadOnly(True)
+        self.CH2En.resize(560,100)
+        self.CH2En.move(20,270)
+
+        # 布局，未看。
+        # hbox = QtGui.QHBoxLayout()
+        # hbox.addStretch(1)
+        # hbox.addWidget(self.okButton)
+        # # hbox.addWidget(self.cancelButton)
+        # vbox = QtGui.QVBoxLayout()
+        # # vbox.addStretch(1)
+        # vbox.addLayout(hbox)
+        # self.setLayout(vbox)
+
+    def fanyi_en2ch(self, event):
+        q = str(self.edit_text.text())
+        fromLang = 'en'  # 从..语言翻译
+        toLang = 'zh'  # 翻译到..语言
+        self.fanyi(fromLang, toLang, q)
+
+    def fanyi_ch2en(self, event):
+        q = str(self.edit_text2.text())
+        fromLang = 'zh'  # 从..语言翻译
+        toLang = 'en'  # 翻译到..语言
+        self.fanyi(fromLang, toLang, q)
+
+    def fanyi(self, fromLang, toLang, q):
+        # q = 'apple'  # 翻译语句
+        salt = random.randint(32768, 65536)  # 随机数
+        sign = appid + q + str(salt) + secretKey  # sign
+        m1 = md5.new()
+        m1.update(sign)
+        sign = m1.hexdigest()  # md5
+        myurl = myurls + '?appid=' + appid + '&q=' + urllib.quote(q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+        try:
+            httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
+            httpClient.request('GET', myurl)
+            response = httpClient.getresponse()
+            response_dict2 = response.read()
+            response_dict2 = json.loads(response_dict2)
+            print response_dict2
+            print response_dict2["trans_result"][0]["dst"]
+            result = response_dict2["trans_result"][0]["dst"]
+            # uni_result = result.decode("unicode-escape")
+            if fromLang == 'en':
+                # print "en"
+                self.fanyi_show_en2ch(result)
+            else:
+                # print "ch"
+                self.fanyi_show_ch2en(result)
+        except Exception, e:
+            print e
+        finally:
+            if httpClient:
+                httpClient.close()
+
+    def fanyi_show_ch2en(self, content):
+        self.CH2En.setText(content)
+        # self.En2CH.adjustSize()
+        self.CH2En.update()
+
+    def fanyi_show_en2ch(self, content):
+        self.En2CH.setText(content)
+        # self.En2CH.adjustSize()
+        self.En2CH.update()
+
+    def center(self):
+        '''
+        使得窗口居中屏幕
+        '''
+        # ===================================== QDesktopWidget ===========================
+        # QtGui.QDesktopWidget这个类提供了用户桌面的信息，包括屏幕大小。
+        qr = self.frameGeometry()                               # 用frameGeometry方法得到了主窗口的矩形框架qr。
+        cp = QtGui.QDesktopWidget().availableGeometry().center()            # 调用这些方法来得到屏幕分辨率，并最终得到屏幕中间点的坐标cp。
+        qr.moveCenter(cp)                                       # 将矩形框架移至屏幕正中央，大小不变。
+        self.move(qr.topLeft())                                 # 窗口移至矩形框架的左上角点，这样应用窗口就位于屏幕的中央了【注意部件的move都是左上角移动到某点】。
+
+    def clickEvents(self, event):
+        '''
+        点击事件，弹出提示信息。
+        '''
+        ev = event.button()
+        if ev == QtCore.Qt.LeftButton:
+            OK = QtGui.QMessageBox.information(self, (u'提示'), (u'左键'), QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+            if OK == QtGui.QMessageBox.Yes:
+                QtGui.QMessageBox.information(self, (u'提示'), (u'YES'), QtGui.QMessageBox.Yes)
+            else:
+                QtGui.QMessageBox.information(self, (u'提示'), (u'NO'), QtGui.QMessageBox.Yes)
+        elif ev == QtCore.Qt.RightButton:
+            OK = QtGui.QMessageBox.warning(self, (u'提示'), (u'右键'), QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+        elif ev == QtCore.Qt.MiddleButton:
+            OK = QtGui.QMessageBox.question(self, (u'提示'), (u'滚动轴'), QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+
+    def closeEvent(self, event):
+        '''
+        重写了这个关闭的方法，跳出提示的信息，不用设置链接按钮
+        '''
+        # ===================================== 提示信息 =================================
+        reply = QtGui.QMessageBox.question(self, u'警告', u"你确定要退出翻译吗?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()              # 这个事件会被接受
+        else:
+            event.ignore()              # 这个事件会被接受
+
+    def btnCloseEvent(self, event):
+        # 目前出了点问题，还没有实现
+        reply = QtGui.QMessageBox.question(self, u'警告', u"你确定要退出翻译吗?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            pass
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    fanyi = fanyiApp()
+    fanyi.show()
+    sys.exit(app.exec_())
