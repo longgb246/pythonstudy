3b98d20d9fd3192a0352a87233a8daba755e4569
longgb246 <lgb453476610@163.com>
2017-03-10 10:18:16 +0800

    修改版本的 SKU 仿真

--- longgb/Work/SKU_simulation/SkuSimulation.py
+++ longgb/Work/SKU_simulation/SkuSimulation.py
@@ -76,44 +76,62 @@ class Utils:
 
 class SkuSimulation:
 
-    def __init__(self, date_range, sales_his, inv_his, sales_pred_mean,vlt_val, vlt_prob,s,S,ito_level=-99,
-                  pred_days=28, sku_id='', sku_name='', org_nation_sale_num_band=None):
+    def __init__(self, date_range, sales_his, inv_his, sales_pred_mean,vlt_val, vlt_prob,s=0,S=0,s_array=[],S_array=[],ito_level=-99,
+                  pred_days=28, sku_id='', sku_name='', org_nation_sale_num_band=None,sale_per=None,cv_sale=0):
         self.sku_id = sku_id
         self.sku_name = sku_name
         self.date_range = date_range
         self.start_dt = datetime.datetime.strptime(self.date_range[0], '%Y-%m-%d')
         self.end_dt = datetime.datetime.strptime(self.date_range[1], '%Y-%m-%d')
         self.simulation_period = (self.end_dt - self.start_dt).days + 1
+        # print 'self.simulation_period .....'
+        # print self.simulation_period
         self.sales_his_origin = sales_his
         self.inv_his = inv_his
         self.org_nation_sale_num_band = org_nation_sale_num_band
+
         self.s=s
         self.S=S
+        self.s_array=s_array
+        self.S_array=S_array
         self.ito_level=ito_level
+        self.sale_per=sale_per
+        self.cv_sale=cv_sale
         # # 销量填充与平滑
-        # 在仿真前对数据进行平滑
+        #在仿真前对数据进行平滑
         self.sales_his = self.sales_his_origin.copy()
         # '''销量平滑，输入DF，返回DF
         #   采用3sigma方法进行平滑处理
         # '''
         # print self.sales_his
         # self.sales_his=EMsmooth.smooth(self.sales_his)
+
         # 预测天数
         self.pred_days = pred_days
         # 销量预测（均值）
         self.sales_pred_mean = sales_pred_mean
+
         # 处理销量预测为空的情况：使用前一天的预测值
         for i in range(self.simulation_period):
-            if self.sales_pred_mean[i] is None:
-                self.sales_pred_mean[i] = self.sales_pred_mean[i - 1]
-        # 处理调拨时长分布情况，针对调拨更多的为两点或者三点分布
+            try:
+                if self.sales_pred_mean[i] is None:
+                    self.sales_pred_mean[i] = self.sales_pred_mean[i - 1]
+            except:
+                print self.sales_pred_mean
+                print self.sku_id
+
+        #处理调拨时长分布情况，针对调拨更多的为两点或者三点分布
         self.vlt_val = vlt_val.astype(np.int32)
         self.vlt_prob = vlt_prob
         self.vlt_distribution = rv_discrete(values=(self.vlt_val, self.vlt_prob))
+
+
+
         # 最晚的到货日期
         self.latest_arrive_index = 0
+
         # 仿真库存，初始化
-        # 初始值=现货库存+在途
+        # TODO：初始值=现货库存+在途
         self.inv_sim = np.array([0] * self.simulation_period, dtype=np.float64)
         self.inv_sim[0] = self.inv_his[0]
         # 仿真销量
@@ -138,7 +156,7 @@ class SkuSimulation:
         self.cur_index = 0
         # 仿真状态：{0: 未开始，1: 进行中，2：正常完成}
         self.simulation_status = 0
-        #self.category = utils.getCategory(self.sales_his)
+        #self.category = Utils.getCategory(self.sales_his)
         self.subCategory = np.array([99]*self.simulation_period)
 
     def reset(self):
@@ -159,7 +177,10 @@ class SkuSimulation:
 
     def calc_lop(self, cr=None):
         demand_mean = np.mean(self.sales_pred_mean[self.cur_index])
-        self.lop[self.cur_index] =self.s*demand_mean
+        if len(self.s_array)>1:
+            self.lop[self.cur_index] =self.s_array[self.cur_index]*demand_mean
+        else:
+            self.lop[self.cur_index] =self.s*demand_mean
         # # 给定VLT，计算VLT期间总销量的均值
         # demand_mean = [sum(self.sales_pred_mean[self.cur_index][:(l+1)]) for l in self.vlt_value_trunc]
         # # VLT期间总销量均值的概率分布
@@ -178,6 +199,7 @@ class SkuSimulation:
         # cur_cr = self.cr_pbs[self.cur_index] if cr is None else cr
         # self.lop[self.cur_index] = np.ceil(part1 + norm.ppf(cur_cr) * math.sqrt(part21 + part22 + 0.1))
 
+
     def calc_replenishment_quantity(self, bp=None, cr=None):
         # cur_bp = self.bp_pbs[self.cur_index] if bp is None else bp
         # sales_pred_sd = self.sales_pred_sd[self.cur_index]
@@ -191,7 +213,12 @@ class SkuSimulation:
         #                norm.ppf(cur_cr) * math.sqrt(cur_bp_var) -
         #                self.inv_sim[self.cur_index] -
         #                self.open_po_sim[self.cur_index])
-        return np.ceil(self.S* np.mean(self.sales_pred_mean[self.cur_index]) -
+        if len(self.S_array)>1:
+            return np.ceil(self.S_array[self.cur_index]* np.mean(self.sales_pred_mean[self.cur_index]) -
+                           self.inv_sim[self.cur_index] -
+                           self.open_po_sim[self.cur_index])
+        else:
+            return np.ceil(self.S* np.mean(self.sales_pred_mean[self.cur_index]) -
                        self.inv_sim[self.cur_index] -
                        self.open_po_sim[self.cur_index])
 
@@ -202,11 +229,12 @@ class SkuSimulation:
         self.vlt_sim[self.cur_index] = self.vlt_distribution.rvs()
         # 更新补货状态，last_index是采购到货天的下标
         last_index = self.cur_index + self.vlt_sim[self.cur_index] + 1
+
         # 判断上次的到货时间点早于该次的到货时间，如果晚于则重新抽样确定vlt
-        # 【疑问？】
         while last_index < self.latest_arrive_index:
             self.vlt_sim[self.cur_index] = self.vlt_distribution.rvs()
             last_index = self.cur_index + self.vlt_sim[self.cur_index] + 1
+
         if last_index < self.simulation_period:
             # 更新采购在途
             self.open_po_sim[(self.cur_index+1):last_index] += self.pur_qtty_sim[self.cur_index]
@@ -242,7 +270,7 @@ class SkuSimulation:
             # 当天仿真销量 = min(当天历史销量, 当天仿真库存量)
             self.sales_sim[self.cur_index] = min(self.sales_his[self.cur_index], self.inv_sim[self.cur_index]+self.open_po_sim[self.cur_index])
             # 下一天的仿真库存量（初始库存） = 当天仿真库存量 - 当天销量,销量可能超过库存因为消耗在途
-            # 将当天的在途进行运算，当天在途数量默认消耗不入库
+            #将当天的在途进行运算，当天在途数量默认消耗不入库
             if self.cur_index < self.simulation_period - 1:
                 self.inv_sim[self.cur_index + 1] = max(self.inv_sim[self.cur_index] -self.sales_sim[self.cur_index],0)
                 self.open_po_sim[self.cur_index]=self.open_po_sim[self.cur_index]-max(0,self.sales_sim[self.cur_index]-self.inv_sim[self.cur_index])
@@ -252,7 +280,28 @@ class SkuSimulation:
 
     def get_daily_data(self):
         if self.simulation_status == 2:
-            daily_data = {'dt': Utils.generate_date_range(self.start_dt, self.end_dt),
+            if len(self.s_array)>1:
+                daily_data = {'dt': Utils.generate_date_range(self.start_dt, self.end_dt),
+                              'sales_his_origin': self.sales_his_origin,
+                              'inv_his': self.inv_his,
+                              'sales_sim': self.sales_sim,
+                              'inv_sim': self.inv_sim,
+                              'lop': self.lop,
+                              'pur_qtty_sim': self.pur_qtty_sim,
+                              'open_po_sim': self.open_po_sim,
+                              'vlt_sim': self.vlt_sim,
+                              'arrive_qtty_sim': self.arrive_qtty_sim,
+                              'sales_pred_mean': self.sales_pred_mean,
+                              'sku_id': np.repeat(self.sku_id, self.simulation_period),
+                              'sale_num_band':self.org_nation_sale_num_band,
+                              'subCategory' : self.subCategory,
+                              's':self.s_array,
+                              'S':self.S_array}
+                return pd.DataFrame(daily_data, columns=['sku_id', 'dt', 'sales_his_origin', 'inv_his', 'sales_sim',
+                                                     'inv_sim', 'lop', 'pur_qtty_sim', 'open_po_sim',
+                                                     'vlt_sim', 'arrive_qtty_sim', 'sales_pred_mean','sale_num_band','subCategory','s','S'])
+            else:
+                daily_data = {'dt': Utils.generate_date_range(self.start_dt, self.end_dt),
                           'sales_his_origin': self.sales_his_origin,
                           'inv_his': self.inv_his,
                           'sales_sim': self.sales_sim,
@@ -266,7 +315,7 @@ class SkuSimulation:
                           'sku_id': np.repeat(self.sku_id, self.simulation_period),
                           'sale_num_band':self.org_nation_sale_num_band,
                           'subCategory' : self.subCategory}
-            return pd.DataFrame(daily_data, columns=['sku_id', 'dt', 'sales_his_origin', 'inv_his', 'sales_sim',
+                return pd.DataFrame(daily_data, columns=['sku_id', 'dt', 'sales_his_origin', 'inv_his', 'sales_sim',
                                                       'inv_sim', 'lop', 'pur_qtty_sim', 'open_po_sim',
                                                      'vlt_sim', 'arrive_qtty_sim', 'sales_pred_mean','sale_num_band','subCategory'])
         else:
@@ -284,8 +333,12 @@ class SkuSimulation:
             ts_sim = np.sum(self.sales_sim)
             ts_his = np.sum(self.sales_his_origin)
             ts_rate=ts_sim/ts_his
-            return [self.sku_id, round(cr_sim,4), round(cr_his,4), round(ito_sim,4), round(ito_his,4), round(ts_sim,4), round(ts_his,4),
-                    self.pur_cnt_sim,self.s,self.S,self.date_range[0],self.date_range[1],self.ito_level,ts_rate]
+            if self.sale_per is None:
+                return [self.sku_id, round(cr_sim,4), round(cr_his,4), round(ito_sim,4), round(ito_his,4), round(ts_sim,4), round(ts_his,4),
+                        self.pur_cnt_sim,self.s,self.S,self.date_range[0],self.date_range[1],self.ito_level,ts_rate,-1,self.cv_sale]
+            else:
+                return [self.sku_id, round(cr_sim,4), round(cr_his,4), round(ito_sim,4), round(ito_his,4), round(ts_sim,4), round(ts_his,4),
+                        self.pur_cnt_sim,self.s,self.S,self.date_range[0],self.date_range[1],self.ito_level,ts_rate,self.sale_per[0],self.cv_sale]
         else:
             print "Please call run_simulation() before calculating KPI!"
 
--- longgb/Work/SKU_simulation/configServer.py
+++ longgb/Work/SKU_simulation/configServer.py
@@ -7,9 +7,10 @@ import numpy as np
 data_dir        ='E:/Allocation_data/'
 output_dir      = 'E:/simulation_results/'
 log_dir         ='E:/Allocation_data/log/'
-data_file_name='sku_allocation_sample2.csv'
+data_file_name='half_month_sale_datasets_20170308221210 (3).csv'
+para_file_name='sample_sku_param_20170221134526.csv'
 # date_range=[['2016-12-01','2016-12-14'],['2016-12-01','2016-12-21'],['2016-12-01','2016-12-28'],['2016-12-01','2017-01-30']]
-date_range=[['2016-12-01','2016-12-15'],['2016-12-16','2016-12-30']]
+date_range=[['2016-12-02','2016-12-15'],['2016-12-16','2016-12-30']]
 date_end='2017-01-30'
 date_start='2016-12-01'
 vlt_val=np.array([1,2])
