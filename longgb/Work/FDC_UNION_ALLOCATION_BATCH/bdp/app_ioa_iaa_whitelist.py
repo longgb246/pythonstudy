56f26b3eb06ad0395af08ae4e28880e6dde05118
longgb246 <lgb453476610@163.com>
2017-04-11 10:18:04 +0800

    标准差sql

--- longgb/Work/FDC_UNION_ALLOCATION_BATCH/bdp/app_ioa_iaa_whitelist.py
+++ longgb/Work/FDC_UNION_ALLOCATION_BATCH/bdp/app_ioa_iaa_whitelist.py
@@ -15,36 +15,27 @@ import time
 sys.path.append(os.getenv('HIVE_TASK'))
 from HiveTask import HiveTask
 
-def get_format_yesterday(num=1,format = '%Y-%m-%d'):
+
+def get_format_yesterday(num=1, format='%Y-%m-%d'):
     """
     获取昨天日期（字符串）默认'%Y-%m-%d'，
     format =‘%d' 获取昨天是本月中第几天
     """
-    into_dt= (datetime.date.today() - datetime.timedelta(num+1)).strftime(format)
-    end_dt  = (datetime.date.today() - datetime.timedelta(num)).strftime(format)
-    return end_dt,into_dt
-
+    into_dt = (datetime.date.today() - datetime.timedelta(num + 1)).strftime(format)
+    end_dt = (datetime.date.today() - datetime.timedelta(num)).strftime(format)
+    return end_dt, into_dt
 
 
 def main():
-
-    if(len(sys.argv)>1): # 传了系统参数
-        into_dt= sys.argv[1]
-        end_dt = (datetime.datetime.strptime(sys.argv[1],'%Y-%m-%d') + datetime.timedelta(1)).strftime( '%Y-%m-%d')
+    if (len(sys.argv) > 1):  # 传了系统参数
+        into_dt = sys.argv[1]
+        end_dt = (datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d') + datetime.timedelta(1)).strftime('%Y-%m-%d')
     else:
         into_dt = get_format_yesterday()[1]
         end_dt = get_format_yesterday()[0]
 
-    # """
-# create table app.app_ioa_iaa_whitelist(
-#     sku_id string,
-#            fdcid string)
-# partitioned by (dt string)
-# stored as TEXTFILE;
-    # """
-
     sql = """
-insert overwrite table app.app_ioa_iaa_whitelist partition(dt='"""+end_dt+"""')
+insert overwrite table app.app_ioa_iaa_whitelist partition(dt='""" + end_dt + """')
 select
         t2.wid sku_id,
         t2.fdcid fdc_id
@@ -55,14 +46,15 @@ select
     on
         t1.dc_id = t2.fdcid
     where
-        t2.start_date <= '"""+into_dt+"""' and
-        t2.end_date > '"""+into_dt+"""' and
+        t2.start_date <= '""" + into_dt + """' and
+        t2.end_date > '""" + into_dt + """' and
         t2.yn = 1 and
         t1.dc_type = 1;
 """
     print(sql)
     ht = HiveTask()
     ht.exec_sql(schema_name='app', sql=sql)
+
 
 if __name__ == "__main__":
     main()
