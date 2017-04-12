ed8772048f669bea9361e12973fd562662ee46b2
longgb246 <lgb453476610@163.com>
2017-04-13 00:12:41 +0800

    Django

---
+++ longgb/Python_Module/Django/Project/longgb246/longgb246/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for longgb246 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "longgb246.settings")
+
+application = get_wsgi_application()
