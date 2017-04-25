14c343b50ada73db07966ebb9ac0b6eb2cfac75a
longgb246 <lgb453476610@163.com>
2017-04-25 17:31:40 +0800

    some changes

--- longgb/Scripts/Script_groupby.py
+++ longgb/Scripts/Script_groupby.py
@@ -29,3 +29,15 @@ plt.style.use('seaborn-dark')
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot([1, 2], [2, 2])
+
+
+# 以 simulationData 为主表。
+# 都是使用 bpData 和 simulationData 的 rdcSku 交的。
+# 那么只可能是  bpData 天数没有。因为 rdcSku 是存在的。
+
+# rdcSkuid     dt             vlt_ref   nrt   bp      safeVLT rdc   skuid      cid2    cid3
+# 10-1031021   2016-10-18      NA       NA    NA      NA      10    1031021    1525    7057
+# 10-1031021   2016-10-19      NA       NA    NA      NA      10    1031021    1525    7057
+# 10-1031021   2017-01-01      NA       NA    NA      NA      10    1031021    1525    7057
+# 10-1092029   2016-10-18      NA       NA    NA      NA      10    1092029    1525    7057
+# 10-1092029   2016-10-19
