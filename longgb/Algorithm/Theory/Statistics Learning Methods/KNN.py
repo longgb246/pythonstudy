9f7b661d42ae8064c9261ff89f97c5281a815cb5
longgb246 <lgb453476610@163.com>
2017-04-28 15:54:33 +0800

    Knn算法

--- longgb/Algorithm/Theory/Statistics Learning Methods/KNN.py
+++ longgb/Algorithm/Theory/Statistics Learning Methods/KNN.py
@@ -2,8 +2,9 @@
 from __future__ import division
 import numpy as np
 import pandas as pd
-from string import Template
 import os
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-darkgrid')
 
 
 def lpDistince(x1, x2, p=1):
@@ -27,13 +28,10 @@ def lpDistinceData():
     lpDistince(x1, x2, p=1)
 
 
-# http://blog.csdn.net/u010551621/article/details/44813299
-
-
 class KDNode:
     def __init__(self, point=None, split=None, left = None, right = None):
         """
-        KD树节点
+        KD树节点，用于存储每一个节点
         point : 数据点
         split : 划分域, split代表划分维度
         left : 左子节点( KDNode 对象)
@@ -47,11 +45,14 @@ class KDNode:
 
 class KDTree():
     def __init__(self):
+        '''
+        KD树建立
+        '''
         self.root = None        # 树的根
         self.dot = None         # 画树图dot文件
         self.__index = None     # 遍历用的
         self.style = '''node [style="filled, rounded", fontname=helvetica] ;\nedge [fontname=helvetica] ;\n'''  # 画树图的风格
-        self.color = ['#FF6347', '#FFA500','#FFEC8B' ,'#9AFF9A' ,'#AEEEEE' ,'#CAE1FF' ,'#B0E2FF','#AB82FF']     # 画树图的颜色列表
+        self.color = ['#EEE9E9', '#FFE4C4','#FFEC8B' ,'#9AFF9A' ,'#AEEEEE' ,'#CAE1FF' ,'#B0E2FF','#AB82FF']     # 画树图的颜色列表
         self.tmp_cont = ''
 
     def __createKDTree(self, data_list):
@@ -91,13 +92,16 @@ class KDTree():
 
     def __drawTree(self, root):
         this_index = self.__index
-        self.cont += 'n{0} [label="{1}", fillcolor="{2}"];\n'.format('0'*(3-len(str(this_index)))+str(this_index),str(list(root.point)),np.random.choice(self.color))
+        point_list = list(root.point)
+        test = ['<font color="red">'+str(x)+'</font>' if i==root.split else str(x) for i,x in enumerate(point_list)]
+        str_need = '['+ reduce(lambda x, y: x+','+y, test) + ' ]'
+        self.cont += 'n{0} [label=<{1}>, fillcolor="{2}"];\n'.format('0'*(3-len(str(this_index)))+str(this_index), str_need, np.random.choice(self.color))
         if root.left:
             self.__index += 1
             next_index  = self.__index
             self.cont += 'n{0} -> n{1};\n'.format('0'*(3-len(str(this_index)))+str(this_index), '0'*(3-len(str(next_index)))+str(next_index))
             self.__drawTree(root.left)
-        else:               # 当发生了空的时候，为了画出是左右节点
+        else:               # 当发生了空的时候，为了画出是左、还是右节点
             self.__index += 1
             next_index = self.__index
             self.tmp_cont += 'edge[color="#FFFF00", style="dashed"]; n{0} [label="", color="#FFFFFF"]'.format('0' * (3 - len(str(next_index))) + str(next_index))
@@ -113,12 +117,12 @@ class KDTree():
             self.__drawTree(root.right)
         else:
             if self.tmp_cont == '':
-                self.tmp_cont = None
                 self.__index += 1
                 next_index = self.__index
                 self.cont += 'edge[color="#FFFF00", style="dashed"]; n{0} [label="", color="#FFFFFF"]'.format('0' * (3 - len(str(next_index))) + str(next_index))
                 self.cont += 'n{0} -> n{1};\n'.format('0' * (3 - len(str(this_index))) + str(this_index),'0' * (3 - len(str(next_index))) + str(next_index))
                 self.cont += 'edge[color=black, style="solid"];'
+            self.tmp_cont = ''
 
     def drawTree(self, is_print=True, save_path=None):
         '''
@@ -137,70 +141,54 @@ class KDTree():
         if is_print:
             print self.dot
 
-
-def findKnn(root, data):
-    """
-    root:KDTree的树根
-    query:查询点
-    return:返回距离data最近的点NN，同时返回最短距离min_dist
-    """
-    data = [2, 3, 4]
-
-    data = np.array(data)
-    root_point = root.point     # 初始化为root的节点
-    min_dist = lpDistince(data, root_point, 2)
-
-    nodeList = []               # 记录路径
-    temp_root = root
-
-    while temp_root:            # 二分查找最近的领域，直到该域叶节点
-        nodeList.append(temp_root)
-        dd = lpDistince(data, temp_root.point, 2)
-        if min_dist > dd:       # 新计算的距离
-            Knn = temp_root.point
-            min_dist = dd
-        ss = temp_root.split
-        if data[ss] <= temp_root.point[ss]:
-            temp_root = temp_root.left
-        else:
-            temp_root = temp_root.right
-
-    while nodeList:             # 回溯查找
-        # 使用list模拟栈，后进先出
-        back_point = nodeList.pop()
-        ss = back_point.split
-        print "back.point = ", back_point.point
-        # 判断是否需要进入父亲节点的子空间进行搜索
-        if abs(data[ss] - back_point.point[ss]) < min_dist:
-            if data[ss] <= back_point.point[ss]:
-                temp_root = back_point.right
+    def __findNN(self, root, data):
+        data = np.array(data)
+        root_point = root.point  # 初始化为root的节点
+        min_dist = lpDistince(data, root_point, 2)
+        nodeList = []  # 记录路径
+        temp_root = root
+        while temp_root:  # 二分查找最近的领域，直到该域叶节点
+            nodeList.append(temp_root)
+            dd = lpDistince(data, temp_root.point, 2)
+            if min_dist > dd:  # 新计算的距离
+                NN = temp_root.point
+                min_dist = dd
+            ss = temp_root.split
+            if data[ss] <= temp_root.point[ss]:
+                temp_root = temp_root.left
             else:
-                temp_root = back_point.left
-            if temp_root:
-                nodeList.append(temp_root)
-                curDist = lpDistince(data, temp_root.point)
-                if min_dist > curDist:
-                    min_dist = curDist
-                    NN = temp_root.point
-    return NN, min_dist
+                temp_root = temp_root.right
+        while nodeList:  # 回溯查找
+            back_point = nodeList.pop()  # 使用list模拟栈，后进先出
+            ss = back_point.split
+            # 判断是否需要进入父亲节点的子空间进行搜索
+            if abs(data[ss] - back_point.point[ss]) < min_dist:  # 如果在该维度下的距离都大于min_dist，则必然不在该域中
+                if data[ss] <= back_point.point[ss]:
+                    temp_root = back_point.right  # 如果是小于，说明自己就在left域，需要到对面的域 - right域里
+                else:
+                    temp_root = back_point.left
+                if temp_root:
+                    nodeList.append(temp_root)
+                    curDist = lpDistince(data, temp_root.point)
+                    if min_dist > curDist:
+                        min_dist = curDist
+                        NN = temp_root.point
+        return NN, min_dist
+
+    def findNN(self, data):
+        """
+        data : 查询的数据，单条数据
+        return : 返回距离data最近的点NN，同时返回最短距离min_dist
+        """
+        NN, min_dist = self.__findNN(self.root ,data)
+        return NN, min_dist
 
 
-def KNN(list, query):
-    min_dist = 9999.0
-    NN = list[0]
-    for pt in list:
-        dist = lpDistince(query, pt)
-        if dist < min_dist:
-            NN = pt
-            min_dist = dist
-    return NN, min_dist
-
-
-def getSampleData():
+def getTrainData():
     '''
-    获取样本数据
+    获取训练样本数据
     '''
-    data_list = [[3, 1, 4],
+    train_data = [[3, 1, 4],
                  [2, 3, 7],
                  [2, 1, 3],
                  [2, 4, 5],
@@ -211,14 +199,78 @@ def getSampleData():
                  [5, 2, 5],
                  [4, 0, 6],
                  [7, 1, 6]]
-    return data_list
+    return train_data
+
+
+def getTrainData2():
+    '''
+    获取训练样本数据，二维空间方便画图观察
+    '''
+    train_data = [[2, 3],
+                 [4, 7],
+                 [5, 4],
+                 [7, 2],
+                 [8, 1],
+                 [9, 6]]
+    train_data_y = [1,0,1,1,0,0]
+    return train_data, train_data_y
+
+
+def getTestData():
+    '''
+    获取测试样本数据
+    '''
+    test_data = [2, 4.5]
+    return test_data
+
+
+def KNN(train_data, train_data_y, test_data, k=1):
+    '''
+    向量化进行 Knn 计算，获取分类类别
+    '''
+    train_data = np.array(train_data)
+    test_data = np.array(test_data)
+    train_data_y = np.array(train_data_y)
+    distince = map(lambda x: list(np.argsort(lpDistince(train_data, x, p=2))[:k]), test_data)
+    type_test = map(lambda x: 1 if np.sum(train_data_y[x])/k > 0.5 else 0, distince)
+    return type_test
+
+
+def plotPoints(data_list, train_data_y, test_data):
+    '''
+    画观察分类的图
+    '''
+    data = pd.concat([pd.DataFrame(data_list), pd.DataFrame(train_data_y)], axis=1)
+    data.columns = ['x1', 'x2', 'y']
+    # fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(data.loc[data['y']==0,['x1']], data.loc[data['y']==0,['x2']], '.', color='red', markersize=12, label='0')
+    ax.plot(data.loc[data['y']==1,['x1']], data.loc[data['y']==1,['x2']], '.', color='blue', markersize=12, label='1')
+    ax.set_xlim([0, 12])
+    ax.set_ylim([0, 9])
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 0.55))
+    ax.plot(test_data[:,0], test_data[:,1], '.', color='black', markersize=12, label='0')
+    plt.show()
+    plt.subplots_adjust(right=0.85)
 
 
 if __name__ == '__main__':
-    data_list = getSampleData()
+    # data_list = getTrainData()
+    data_list, train_data_y = getTrainData2()
+    test_data = getTestData()
+
+    # 使用 KD 树
     KDTree_ins = KDTree()
-    root = KDTree_ins.createKDTree(data_list)
-    KDTree_ins.preOrder()
-    KDTree_ins.drawTree(save_path=r'D:\Lgb\Self\Learn\check.dot')
-    os.system('D:/Lgb/Softwares/graphviz/bin/dot -Tpng D:/Lgb/Self/Learn/check.dot -o D:/Lgb/Self/Learn/check2.png')
-    pass
+    root = KDTree_ins.createKDTree(data_list)       # 生成树
+    KDTree_ins.preOrder()                           # 前序遍历
+    KDTree_ins.drawTree(save_path=r'D:\Lgb\Self\Learn\check.dot', is_print=False)           # 生成画图文档
+    os.system('D:/Lgb/Softwares/graphviz/bin/dot -Tpng D:/Lgb/Self/Learn/check.dot -o D:/Lgb/Self/Learn/check2.png')        # 画图
+    NN, min_dist = KDTree_ins.findNN(test_data)
+    print 'Nearest Distance of {0} is {1}'.format(test_data, list(NN))
+    print 'Nearest Distance is : {0}'.format(min_dist)
+
+    # 直接计算使用 knn 的向量化计算
+    results = KNN(data_list, train_data_y, test_data, k=3)
+    plotPoints(data_list, train_data_y, test_data)
+
--- longgb/Work/Replenishment/Online/simulation.R
+++ longgb/Work/Replenishment/Online/simulation.R
@@ -1,73 +1,85 @@
 library(data.table)
-############################################
-####Load the data and preprocessing
+################################################
+####  Load the data and preprocessing
 ################################################
 forecastResult  <- fread("forecastInput.csv", integer64="numeric", na.strings=c("NULL","NA", "", "\\N"))
 bpData          <- fread("bp.csv", integer64="numeric", na.strings=c("NULL","NA", "", "\\N"))
-
 vltDD           <- fread("vltDDInput.csv", integer64="numeric", na.strings=c("NULL","NA", "", "\\N"))
 
-###The vlt data preprocessing
-vltDD[, rdc:=tstrsplit(metaFlag,'-')[3]]
-vltDD[, supplier:=tstrsplit(metaFlag,'-')[2]]
-vltDD[,rdcSkuid:=paste(rdc, sku_id, sep='-')]
-vltDD   <- unique(vltDD, by=c("rdcSkuid", "intVlt"))
-vltDD               <- vltDD[rdcSkuid %in% intersectRdcSkuid,]
 
-#####  bp data preprocess
-bpData[, rdcSkuid:=paste(int_org_num, sku_id, sep='-')]
-bpData[, dt:=as.Date(dt)]
-#Check if oneday get multiple record
-#bpData$index    <- 1:nrow(bpData)
-#duplicatedCheck <- bpData[,list(recordCount=length(index)), by=c("rdcSkuid", "dt")]
-bpData  	<- bpData[,.(rdcSkuid, dt,vlt_ref, nrt, bp)]
-
-#####The forecast result checking
-forecastResult      <- forecastResult[rdcSkuid %in% intersectRdcSkuid,]
+####  The forecast data preprocessing
+forecastResult[, salesFill:=as.integer(ceiling(mean(sales,na.rm=T))), by="rdcSkuid"]
+forecastResult[is.na(sales), sales:=salesFill]
+forecastResult[is.na(sales), sales:=0]
+forecastResult[, curDate:=as.Date(curDate)]
 forecastResult$index    <- 1:nrow(forecastResult)
 forecastResult      <- unique(forecastResult, by=c("rdcSkuid", "curDate"))
-forecastResult[,curDate:=as.Date(curDate)]
 
-######If pred<0, assign 0 to pred
-#1-10
+
+####  The vlt data preprocessing
+vltDD[, rdc:=tstrsplit(metaFlag,'-')[3]]
+vltDD[, supplier:=tstrsplit(metaFlag,'-')[2]]
+vltDD[, rdcSkuid:=paste(rdc, sku_id, sep='-')]
+vltDD   <- unique(vltDD, by=c("rdcSkuid", "intVlt"))
+
+
+####  The bp data preprocess
+bpData[, rdcSkuid:=paste(int_org_num, sku_id, sep='-')]
+bpData[, dt:=as.Date(dt)]
+# Check if oneday get multiple record
+# bpData$index    <- 1:nrow(bpData)
+# duplicatedCheck <- bpData[,list(recordCount=length(index)), by=c("rdcSkuid", "dt")]
+bpData  <- bpData[, .(rdcSkuid, dt,vlt_ref, nrt, bp)]
+
+
+####  Get the same rdcSkuid
+intersectRdcSkuid   <- intersect(unique(vltDD$rdcSkuid), unique(forecastResult$rdcSkuid))
+forecastResult      <- forecastResult[rdcSkuid %in% intersectRdcSkuid,]
+intersectRdcSkuid   <- intersect(unique(bpData$rdcSkuid), unique(forecastResult$rdcSkuid))
+bpData              <- bpData[rdcSkuid %in% intersectRdcSkuid,]
+vltDD               <- vltDD[rdcSkuid %in% intersectRdcSkuid,]
+forecastResult      <- forecastResult[rdcSkuid %in% intersectRdcSkuid,]
+
+
+####  If pred<0, assign 0 to pred
+# 1-10
 forecastResult[predMean1<0, predMean1:=0];forecastResult[predMean2<0, predMean2:=0];
 forecastResult[predMean3<0, predMean3:=0];forecastResult[predMean4<0, predMean4:=0];
 forecastResult[predMean5<0, predMean5:=0];forecastResult[predMean6<0, predMean6:=0];
 forecastResult[predMean7<0, predMean7:=0];forecastResult[predMean8<0, predMean8:=0];
 forecastResult[predMean9<0, predMean9:=0];forecastResult[predMean10<0, predMean10:=0];
-#11-20
+# 11-20
 forecastResult[predMean11<0, predMean11:=0];forecastResult[predMean12<0, predMean12:=0];
 forecastResult[predMean13<0, predMean13:=0];forecastResult[predMean14<0, predMean14:=0];
 forecastResult[predMean15<0, predMean15:=0];forecastResult[predMean16<0, predMean16:=0];
 forecastResult[predMean17<0, predMean17:=0];forecastResult[predMean18<0, predMean18:=0];
 forecastResult[predMean19<0, predMean19:=0];forecastResult[predMean20<0, predMean20:=0];
-#21-28
+# 21-28
 forecastResult[predMean21<0, predMean21:=0];forecastResult[predMean22<0, predMean22:=0];
 forecastResult[predMean23<0, predMean23:=0];forecastResult[predMean24<0, predMean24:=0];
 forecastResult[predMean25<0, predMean25:=0];forecastResult[predMean26<0, predMean26:=0];
 forecastResult[predMean27<0, predMean27:=0];forecastResult[predMean28<0, predMean28:=0];
 
-intersectRdcSkuid   <- intersect(unique(bpData$rdcSkuid), unique(forecastResult$rdcSkuid))
-forecastResult      <- forecastResult[rdcSkuid %in% intersectRdcSkuid,]
-bpData              <- bpData[rdcSkuid %in% intersectRdcSkuid,]
 
-######################################################3
-#### Simulation framework
-#(1) forecast data preprocessing 
-######################################################3
+################################################
+####  Simulation framework
+#  (1) Forecast data preprocessing
+################################################
 simulateInitialDate   <- as.Date("2016-10-01")
 simulationData  <- forecastResult[curDate>=simulateInitialDate,]
 
-####make the intial date get the data
+#### Make the intial date get the data
 beginRdcSkuidList   <- simulationData[curDate==simulateInitialDate, unique(rdcSkuid)]
 simulationData      <- simulationData[rdcSkuid %in% beginRdcSkuidList,]
 
+
+#### Delete some data
 simulationData[, c("sumSales","sumInventory"):=list(sum(sales,na.rm=T), sum(inventory,na.rm=T)), by=rdcSkuid]
-#The initial inventory is zero
+# The initial inventory is zero
 removeRdcSku1   <- unique(simulationData[curDate==simulateInitialDate & is.na(inventory),]$rdcSkuid)
-#The initial forecast result is zero
+# The initial forecast result is zero
 removeRdcSku2   <- unique(simulationData[curDate==simulateInitialDate & is.na(predMean1), ]$rdcSkuid)
-#The total inventory is zero
+# The total inventory is zero
 removeRdcSku3   <- unique(simulationData[sumInventory==0,]$rdcSkuid)
 # The total sales is zero
 removeRdcSku4   <- unique(simulationData[sumSales==0,]$rdcSkuid)
@@ -76,7 +88,8 @@ removeRdSkuList     <- unique(c(removeRdcSku1, removeRdcSku2, removeRdcSku3,remo
 simulationData  <- simulationData[!rdcSkuid %in% removeRdSkuList,]
 simulationData  <- simulationData[order(rdcSkuid, curDate),]
 
-###The calculation of safe vlt
+
+#### The calculation of safe vlt
 bpData[, safeVLT:=vlt_ref+nrt]
 bpData[safeVLT<14, safeVLT:=14]
 
@@ -85,6 +98,18 @@ setkeyv(simulationData, c("rdcSkuid", "curDate"))
 
 simulationData  <- bpData[simulationData]
 simulationData  <- simulationData[!is.na(bp),]
+
+
+# ==================================================================
+# 10-1093041 2016-10-18
+bpData[rdcSkuid=="10-1093041" & dt=="2016-10-18", ]
+bpData[rdcSkuid=="10-1093041", .(rdcSkuid, dt)]
+bpDataFinal[rdcSkuid=="10-1093041", .(rdcSkuid, dt)]
+bpDataFinal[rdcSkuid=="10-1093041" & dt=="2016-10-18", .(rdcSkuid, dt, bp, vlt_ref, nrt)]
+bpDataMissing[rdcSkuid=="10-1093041", .(rdcSkuid, dt)]
+subData[rdcSkuid=="10-1093041", .(rdcSkuid, dt, bp, vlt_ref, nrt)]
+# ==================================================================
+
 
 safeVLTList     <- unique(simulationData$safeVLT)
 LOPList     <- lapply(safeVLTList, function(x){
